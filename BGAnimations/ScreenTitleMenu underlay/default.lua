5ba1f4331e252799344ead51f8fcd8a6e05db197
Ekaterina Ageeva <sereni.nm@gmail.com>
2018-12-02 21:03:37 +0000

    Vertical support in ScreenTitleMenu underlay.\nThis affects only the logo and song stats, menu items and bottom panel will be  moved separately.

--- BGAnimations/ScreenTitleMenu underlay/default.lua
+++ BGAnimations/ScreenTitleMenu underlay/default.lua
@@ -28,6 +28,20 @@ else
 	sm_version = ProductID() .. sm_version
 end
 -- - - - - - - - - - - - - - - - - - - - -
+-- Constants defining UI element positions depending on horizontal or vertical mode.
+local is_horizontal = GetScreenAspectRatio() > 1
+local song_stats_zoom 		= (is_horizontal and 0.8 or 0.67)
+local song_stats_y 			= (is_horizontal and -120 or -75)
+local logos_x 				= (is_horizontal and 0 or 2)
+local logos_y 				= (is_horizontal and -16 or -10)
+local logos_zoom_itg 		= (is_horizontal and 0.2 or 0.114)
+local logos_zoom_pump		= (is_horizontal and 0.205 or 0.125)
+local title_zoom 			= (is_horizontal and 0.7 or 0.4)
+local hat_x 				= (is_horizontal and 130 or 74)
+local hat_decelerate_rate 	= (is_horizontal and 1.333 or 1.1)
+local hat_decelerate_y		= (is_horizontal and -110 or -65)
+
+-- - - - - - - - - - - - - - - - - - - - -
 local image = ThemePrefs.Get("VisualTheme")
 
 if image == "Spooky" then  --SSHHHH dont tell anyone ;)
@@ -43,8 +57,9 @@ local af = Def.ActorFrame{
 	end,
 	OffCommand=cmd(linear,0.5; diffusealpha, 0),
 
+	-- Program version and song stats
 	Def.ActorFrame{
-		InitCommand=function(self) self:zoom(0.8):y(-120):diffusealpha(0) end,
+		InitCommand=function(self) self:zoom(song_stats_zoom):y(song_stats_y):diffusealpha(0) end,
 		OnCommand=function(self) self:sleep(0.2):linear(0.4):diffusealpha(1) end,
 
 		Def.BitmapText{
@@ -59,14 +74,16 @@ local af = Def.ActorFrame{
 		}
 	},
 
+	-- Arrows between SIMPLY and LOVE
 	LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
 		InitCommand=function(self)
-			self:y(-16):zoom( game=="pump" and 0.2 or 0.205 )
+			self:xy(logos_x,logos_y):zoom( game=="pump" and logos_zoom_itg or logos_zoom_pump )
 		end
 	},
 
+	-- Large SIMPLY LOVE title
 	LoadActor("Simply".. image .." (doubleres).png") .. {
-		InitCommand=function(self) self:x(2):zoom(0.7):shadowlength(0.75) end,
+		InitCommand=function(self) self:x(2):zoom(title_zoom):shadowlength(0.75) end,
 		OffCommand=function(self) self:linear(0.5):shadowlength(0) end
 	}
 }
@@ -75,8 +92,8 @@ local af = Def.ActorFrame{
 if PREFSMAN:GetPreference("EasterEggs") and MonthOfYear()==11 then
 	af[#af+1] = Def.Sprite{
 		Texture=THEME:GetPathB("ScreenTitleMenu", "underlay/hat.png"),
-		InitCommand=function(self) self:zoom(0.225):xy( 130, -self:GetHeight()/2 ):rotationz(15):queuecommand("Drop") end,
-		DropCommand=function(self) self:decelerate(1.333):y(-110) end,
+		InitCommand=function(self) self:zoom(0.15):xy( hat_x, -self:GetHeight()/2 ):rotationz(15):queuecommand("Drop") end,
+		DropCommand=function(self) self:decelerate(hat_decelerate_rate):y(hat_decelerate_y) end,
 	}
 end
 
