c17a20f16b36f60087808714558a193965e4f6b3
Sereni <sereni.nm@gmail.com>
2019-03-28 21:59:09 +0000

    Stretch song progress bar to take up all available space on the top row

--- BGAnimations/ScreenGameplay underlay/Shared/SongInfoBar.lua
+++ BGAnimations/ScreenGameplay underlay/Shared/SongInfoBar.lua
@@ -3,22 +3,22 @@ return Def.ActorFrame{
 	-- Song Completion Meter
 	Def.ActorFrame{
 		Name="SongMeter",
-		InitCommand=cmd(xy, _screen.cx, 20 ),
+		InitCommand=cmd(xy, _screen.cx-17, 20 ),
 
 		Def.SongMeterDisplay{
-			StreamWidth=_screen.w/2-10,
+			StreamWidth=_screen.w-50,
 			Stream=Def.Quad{ 
 				InitCommand=cmd(zoomy,18; diffuse,DifficultyIndexColor(2))
 			}
 		},
 
-		Border( _screen.w/2-10, 22, 2 ),
+		Border( _screen.w-50, 22, 2 ),
 	},
 
 	-- Song Title
 	LoadFont("_miso")..{
 		Name="SongTitle",
-		InitCommand=cmd(zoom,0.8; shadowlength,0.6; maxwidth, _screen.w/2.5 - 10; xy, _screen.cx, 20 ),
+		InitCommand=cmd(zoom,0.8; shadowlength,0.6; maxwidth, _screen.w - 50; xy, _screen.cx-17, 20 ),
 		CurrentSongChangedMessageCommand=cmd(playcommand, "Update"),
 		UpdateCommand=function(self)
 			local song = GAMESTATE:GetCurrentSong()
