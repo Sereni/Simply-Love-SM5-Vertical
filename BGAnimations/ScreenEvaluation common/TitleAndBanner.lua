57477d497f6439122f303aec617f679c4c3252c7
Sereni <sereni.nm@gmail.com>
2019-03-30 20:15:10 +0000

    Make banner smaller on ScreenEvaluation

--- BGAnimations/ScreenEvaluation common/TitleAndBanner.lua
+++ BGAnimations/ScreenEvaluation common/TitleAndBanner.lua
@@ -4,12 +4,12 @@ local af = Def.ActorFrame{
 
 	-- quad behind the song/course title text
 	Def.Quad{
-		InitCommand=cmd(diffuse,color("#1E282F"); xy,_screen.cx, 54.5; zoomto, 292.5,20),
+		InitCommand=cmd(diffuse,color("#1E282F"); xy,_screen.cx, 54.5; zoomto, 209.5,20),
 	},
 
 	-- song/course title text
 	LoadFont("_miso")..{
-		InitCommand=cmd(xy,_screen.cx,54; maxwidth, 294 ),
+		InitCommand=cmd(xy,_screen.cx,54; maxwidth, 209 ),
 		OnCommand=function(self)
 			local songtitle = (GAMESTATE:IsCourseMode() and GAMESTATE:GetCurrentCourse():GetDisplayFullTitle()) or GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
 
@@ -33,12 +33,12 @@ if SongOrCourse and SongOrCourse:HasBanner() then
 				self:LoadFromSong( GAMESTATE:GetCurrentSong() )
 			end
 		end,
-		OnCommand=cmd(xy, _screen.cx, 121.5; setsize,418,164; zoom, 0.7 )
+		OnCommand=cmd(xy, _screen.cx, 105.5; setsize,418,164; zoom, 0.5 )
 	}
 else
 	--fallback banner
 	af[#af+1] = LoadActor( THEME:GetPathB("ScreenSelectMusic", "overlay/colored_banners/" .. (banner_directory[ThemePrefs.Get("VisualTheme")] or "Hearts") .. "/banner" .. SL.Global.ActiveColorIndex .. " (doubleres).png"))..{
-		InitCommand=function(self) self:xy( _screen.cx, 121.5):zoom(0.7) end
+		InitCommand=function(self) self:xy( _screen.cx, 121.5):zoom(0.5) end
 	}
 end
 
