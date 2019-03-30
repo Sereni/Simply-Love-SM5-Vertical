8e12b6e144d1a938829ed37e6d7b8f999212b086
Sereni <sereni.nm@gmail.com>
2019-03-30 20:19:08 +0000

    Scale music rate bar to banner size and move it up

--- BGAnimations/ScreenEvaluation common/RateMod.lua
+++ BGAnimations/ScreenEvaluation common/RateMod.lua
@@ -2,7 +2,7 @@ return Def.ActorFrame{
 	-- don't bother drawing the ActorFrame if MusicRate==1
 	InitCommand=function(self)
 		self:visible(SL.Global.ActiveModifiers.MusicRate ~= 1)
-			:xy(_screen.cx, 172):zoom(0.7)
+			:xy(_screen.cx, 151):zoom(0.5)
 	end,
 
 	--quad behind the MusicRate text
