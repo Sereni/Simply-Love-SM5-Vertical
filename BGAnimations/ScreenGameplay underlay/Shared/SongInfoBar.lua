d560880ae7ba1ef278374f1767e8e001810dc09b
Sereni <sereni.nm@gmail.com>
2020-01-25 16:55:05 +0000

    [4.8.6 cleanup] Fix song progress bar color and coordinates

--- BGAnimations/ScreenGameplay underlay/Shared/SongInfoBar.lua
+++ BGAnimations/ScreenGameplay underlay/Shared/SongInfoBar.lua
@@ -3,31 +3,24 @@ local yOffset = 20
 local barLength = _screen.w-45
 
 return Def.ActorFrame{
-	-- Song Completion Meter
-	Def.ActorFrame{
-		Name="SongMeter",
-		InitCommand=cmd(xy, xOffset, yOffset ),
+	Name="SongMeter",
+	InitCommand=function(self) self:xy(xOffset, yOffset) end,
 
-		Def.SongMeterDisplay{
-			StreamWidth=barLength,
-			Stream=Def.Quad{
-				InitCommand=cmd(zoomy,18; diffuse,DifficultyIndexColor(2))
-			}
-		},
-
-		Border( barLength, 22, 2 ),
+	Def.SongMeterDisplay{
+		StreamWidth=(barLength),
+		Stream=Def.Quad{ InitCommand=function(self) self:zoomy(18):diffuse(GetCurrentColor()) end }
 	},
 
-	Border( _screen.w/2-10, 22, 2 ),
+	Border( barLength, 22, 2 ),
 
 	-- Song Title
 	LoadFont("Common Normal")..{
 		Name="SongTitle",
-		InitCommand=cmd(zoom,0.8; shadowlength,0.6; maxwidth, barLength; xy, xOffset, yOffset ),
-		CurrentSongChangedMessageCommand=cmd(playcommand, "Update"),
-		UpdateCommand=function(self)
+		InitCommand=function(self) self:zoom(0.8):shadowlength(0.6):maxwidth(barLength) end,
+		CurrentSongChangedMessageCommand=function(self)
 			local song = GAMESTATE:GetCurrentSong()
 			self:settext( song and song:GetDisplayFullTitle() or "" )
 		end
+
 	}
 }
