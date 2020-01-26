ffaef3ac5df462e12afd32c8ce68e1e2e010b70b
Sereni <sereni.nm@gmail.com>
2020-01-26 12:42:12 +0000

    Change records pane size to fit, and cut at 8 records

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
@@ -4,11 +4,11 @@ local pane = Def.ActorFrame{
 	Name="Pane3",
 	InitCommand=function(self)
 		self:visible(false)
-		self:y(_screen.cy - 62):zoom(0.8)
+		self:y(_screen.cy - 53):zoom(0.65)
 	end
 }
 
-local args = { Player=player, NumHighScores=10, RoundsAgo=1 }
+local args = { Player=player, NumHighScores=8, RoundsAgo=1 }
 
 pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)
 
