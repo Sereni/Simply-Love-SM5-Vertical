c2ce0a0d46711ade8aef0a8452464b2dcd8e0b24
Sereni <sereni.nm@gmail.com>
2020-02-27 19:36:18 +0000

    [DontJudgeMe] Show an otter picture instead of Pane1 :3

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/default.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane1/default.lua
@@ -1,7 +1,13 @@
 local player = ...
 local mods = SL[ToEnumShortString(player)].ActiveModifiers
 -- No judgement in DoNotJudgeMe mode.
-if mods.DoNotJudgeMe then return end
+if mods.DoNotJudgeMe then
+  return Def.ActorFrame{
+		Name="Pane1",
+		InitCommand=cmd(xy, 0, 250),
+		LoadActor("otter.png")..{ OnCommand=function(self) self:zoomto(210,130) end }
+	}
+end
 
 return Def.ActorFrame{
 	Name="Pane1",
