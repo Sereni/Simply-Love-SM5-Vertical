6ec8d32bf65451b77db42ee05b522f60f4183af7
Sereni <sereni.nm@gmail.com>
2020-04-11 21:59:43 +0100

    [SelectMusic] Correct misaligned background quads behind pack names

--- Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
+++ Graphics/MusicWheelItem SectionCollapsed NormalPart.lua
@@ -4,6 +4,6 @@ local WheelWidth = THEME:GetMetric("MusicWheel", "WheelWidth")
 return Def.ActorFrame{
 	InitCommand=function(self) self:x(26) end,
 
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumWheelItems) end },
+	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end },
 	Def.Quad{ InitCommand=function(self) self:diffuse(color("#283239")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end }
 }
--- Graphics/MusicWheelItem SectionExpanded NormalPart.lua
+++ Graphics/MusicWheelItem SectionExpanded NormalPart.lua
@@ -4,6 +4,6 @@ local WheelWidth = THEME:GetMetric("MusicWheel", "WheelWidth")
 return Def.ActorFrame{
 	InitCommand=function(self) self:x(26) end,
 
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumWheelItems) end },
+	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end },
 	Def.Quad{ InitCommand=function(self) self:diffuse(color("#4c565d")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end }
 }
