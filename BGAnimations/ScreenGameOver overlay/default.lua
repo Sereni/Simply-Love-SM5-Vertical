f9bb812065a7b554ae8e07a54d6508d0dc665265
Sereni <sereni.nm@gmail.com>
2020-04-25 18:53:18 +0100

    Lay out ScreenGameOver

--- BGAnimations/ScreenGameOver overlay/default.lua
+++ BGAnimations/ScreenGameOver overlay/default.lua
@@ -3,44 +3,44 @@ local Players = GAMESTATE:GetHumanPlayers();
 local t = Def.ActorFrame{
 	LoadFont("_wendy white")..{
 		Text="GAME",
-		InitCommand=function(self) self:xy(_screen.cx,_screen.cy-40):croptop(1):fadetop(1):zoom(1.2):shadowlength(1) end,
+		InitCommand=function(self) self:xy(_screen.cx,_screen.cy-85):croptop(1):fadetop(1):zoom(1):shadowlength(1) end,
 		OnCommand=function(self) self:decelerate(0.5):croptop(0):fadetop(0):glow(1,1,1,1):decelerate(1):glow(1,1,1,1) end,
 		OffCommand=function(self) self:accelerate(0.5):fadeleft(1):cropleft(1) end
 	},
 	LoadFont("_wendy white")..{
 		Text="OVER",
-		InitCommand=function(self) self:xy(_screen.cx,_screen.cy+40):croptop(1):fadetop(1):zoom(1.2):shadowlength(1) end,
+		InitCommand=function(self) self:xy(_screen.cx,_screen.cy-15):croptop(1):fadetop(1):zoom(1):shadowlength(1) end,
 		OnCommand=function(self) self:decelerate(0.5):croptop(0):fadetop(0):glow(1,1,1,1):decelerate(1):glow(1,1,1,1) end,
 		OffCommand=function(self) self:accelerate(0.5):fadeleft(1):cropleft(1) end
 	},
 
-	--Player 1 Stats BG
+	-- Stats BG
 	Def.Quad{
 		InitCommand=function(self)
-			self:zoomto(160,_screen.h):xy(80, _screen.h/2):diffuse(color("#00000099"))
+			self:zoomto(_screen.w,_screen.h/4):xy(_screen.w/2, _screen.h-_screen.h/4/2):diffuse(color("#00000099"))
 			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
 		end,
 	},
-
-	--Player 2 Stats BG
-	Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(160,_screen.h):xy(_screen.w-80, _screen.h/2):diffuse(color("#00000099"))
-			if ThemePrefs.Get("RainbowMode") then self:diffuse(color("#000000dd")) end
-		end,
-	}
 }
 
 for player in ivalues(Players) do
 
-	local line_height = 60
-	local middle_line_y = 220
-	local x_pos = player == PLAYER_1 and 80 or _screen.w-80
+	local line_height = 30
+	local x_offset = 60
+	local y_offset = _screen.h-_screen.h/4
+	local stat_zoom = 0.6
+	local max_width = 200
+	local x_col1 = _screen.cx
+	local x_col2 = _screen.cx
 	local PlayerStatsAF = Def.ActorFrame{ Name="PlayerStatsAF_"..ToEnumShortString(player) }
 	local stats
 
 	-- first, check if this player is using a profile (local or MemoryCard)
 	if PROFILEMAN:IsPersistentProfile(player) then
+
+	  -- Change layout to accommodate two columns
+		x_col1 = _screen.cx - x_offset
+		x_col2 = _screen.cx + x_offset
 
 		-- if a profile is in use, grab gameplay stats for this session that are pertinent
 		-- to this specific player's profile (highscore name, calories burned, total songs played)
@@ -52,22 +52,14 @@ for player in ivalues(Players) do
 				Text=stat,
 				InitCommand=function(self)
 					self:diffuse(PlayerColor(player))
-						:xy(x_pos, (line_height*(i-1)) + 40)
-						:maxwidth(150)
+						:xy(x_col1, (line_height*(i)) + y_offset)
+						:maxwidth(max_width)
+						:zoom(stat_zoom)
 				end
 			}
 		end
 
 	end
-
-	-- draw a thin line (really just a Def.Quad) separating
-	-- the upper (profile) stats from the lower (general) stats
-	PlayerStatsAF[#PlayerStatsAF+1] = Def.Quad{
-		InitCommand=function(self)
-			self:zoomto(120,1):xy(x_pos, middle_line_y)
-				:diffuse( PlayerColor(player) )
-		end
-	}
 
 	-- retrieve general gameplay session stats for which a profile is not needed
 	stats = LoadActor("PlayerStatsWithoutProfile.lua", player)
@@ -78,8 +70,9 @@ for player in ivalues(Players) do
 			Text=stat,
 			InitCommand=function(self)
 				self:diffuse(PlayerColor(player))
-					:xy(x_pos, (line_height*i) + middle_line_y)
-					:maxwidth(150)
+					:xy(x_col2, (line_height*i) + y_offset)
+					:maxwidth(max_width)
+					:zoom(stat_zoom)
 			end
 		}
 	end
