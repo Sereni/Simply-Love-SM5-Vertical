3cff6c261d2e972f2dd66bb275f4513fc50b1abd
Sereni <sereni.nm@gmail.com>
2020-06-27 12:46:39 +0100

    [ScreenSelectMusic] Fix alignment of the music wheel
    - Change the alignment of the whole wheel from the center back to the portrait value
    - Change alignment of the base quad to a different constant — I couldn't find what other constant changed to make it misaligned, but that fixes it
    - Remove all references to NumVisibleItems — this was breaking quad thickness
    - Remove a block in metrics.ini that has no effect on placement as far as I could tell
    - Remove special color for one specific song which isn't even MAX300...

--- Graphics/MusicWheel highlight.lua
+++ Graphics/MusicWheel highlight.lua
@@ -1,12 +1,8 @@
 local NumWheelItems = THEME:GetMetric("MusicWheel", "NumWheelItems")
--- subtract 2 from the total number of MusicWheelItems
--- one MusicWheelItem will be offsceen above, one will be offscreen below
-local NumVisibleItems = NumWheelItems - 2
-
 local WheelWidth = THEME:GetMetric("MusicWheel", "WheelWidth")
 
 return Def.Quad{
     InitCommand=function(self)
-        self:zoomto(WheelWidth,_screen.h/(NumVisibleItems) - 1):x(26)
+        self:zoomto(WheelWidth,_screen.h/(NumWheelItems) - 1):x(26)
     end
 }
--- Graphics/MusicWheelItem SectionExpanded NormalPart.lua
+++ Graphics/MusicWheelItem SectionExpanded NormalPart.lua
@@ -1,13 +1,10 @@
 local NumWheelItems = THEME:GetMetric("MusicWheel", "NumWheelItems")
--- subtract 2 from the total number of MusicWheelItems
--- one MusicWheelItem will be offsceen above, one will be offscreen below
-local NumVisibleItems = NumWheelItems - 2
 local WheelWidth = THEME:GetMetric("MusicWheel", "WheelWidth")
 
 return Def.ActorFrame{
 	-- the MusicWheel is centered via metrics under [ScreenSelectMusic]; offset by a slight amount to the right here
 	InitCommand=function(self) self:x(26) end,
 
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumVisibleItems - 1) end },
-	Def.Quad{ InitCommand=function(self) self:diffuse(color("#4c565d")):zoomto(WheelWidth, _screen.h/NumVisibleItems - 1) end }
+	Def.Quad{ InitCommand=function(self) self:diffuse(color("#000000")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end },
+	Def.Quad{ InitCommand=function(self) self:diffuse(color("#4c565d")):zoomto(WheelWidth, _screen.h/NumWheelItems - 1) end }
 }
