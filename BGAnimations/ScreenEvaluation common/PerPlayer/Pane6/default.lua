2ff9fa707dcc6ea79567022e97f455c0a5cf9bd1
Sereni <sereni.nm@gmail.com>
2020-07-01 07:31:49 +0100

    Fix QR pane alignment
    Also two meaningful changes:
    - Reduce alpha of the huge aggressive red cross so that it no longer induces feelings of utter failure. But seriously, now the QR code behind it works and the cool easter egg in it is discoverable!
    - Display only the first two failure reasons. This is how many fit on the pane, and others would have been masked anyway.

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane6/default.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane6/default.lua
@@ -29,9 +29,12 @@ if ValidForGrooveStats then
 	text = ScreenString("QRInstructions")
 else
 	url = "https://www.youtube.com/watch?v=FMABVVk4Ge4"
+	num_failed_checks = 0
+	max_checks_to_display = 2
 
 	for i, passed_check in ipairs(checks) do
-		if passed_check == false then
+		if passed_check == false and num_failed_checks < max_checks_to_display then
+			num_failed_checks = num_failed_checks + 1
 			-- the 3rd check is GameMode (ITG, FA+, Casual, etc.)
 			if i==3 then
 				-- that string has a %s token which so we can pass in the current SL GameMode
@@ -50,7 +53,7 @@ local qrcode_size = 122
 
 local pane = Def.ActorFrame{
 	Name="Pane6",
-	InitCommand=function(self) self:visible(false):xy(-140, 222) end,
+	InitCommand=function(self) self:visible(false) end,
 	ShrinkCommand=function(self)
 		if self:GetVisible() and not ValidForGrooveStats and not X_HasBeenBlinked then
 			self:queuecommand("BlinkX")
@@ -62,10 +65,12 @@ local text_x_offset = -100
 local text_y_offset = 225
 local text_width = 72
 local text_body_size = 0.55
+local code_x_offset = -21
+local code_y_offset = 189
 
 pane[#pane+1] = qrcode_amv( url, qrcode_size )..{
 	OnCommand=function(self)
-		self:xy(-21,189)
+		self:xy(code_x_offset,code_y_offset)
 	end
 }
 
@@ -73,19 +78,17 @@ pane[#pane+1] = qrcode_amv( url, qrcode_size )..{
 if not ValidForGrooveStats then
 	pane[#pane+1] = LoadActor("x.png")..{
 		InitCommand=function(self)
-			self:zoom(1):xy(-17,193):align(0,0)
+			self:zoom(0.77):align(0,0):xy(code_x_offset,code_y_offset)
 		end,
 		-- blink the red X once when the player first toggles into the QR pane
 		BlinkXCommand=function(self)
 			X_HasBeenBlinked = true
-			self:finishtweening():sleep(0.25):linear(0.3):diffusealpha(0):sleep(0.175):linear(0.3):diffusealpha(1)
+			self:finishtweening():sleep(0.25):linear(0.3):diffusealpha(0):sleep(0.175):linear(0.3):diffusealpha(0.5)
 		end
 	}
 end
 
-pane[#pane+1] = LoadActor("../Pane2/Percentage.lua", player)..{
-	OnCommand=function(self) self:xy(25, -22) end
-}
+pane[#pane+1] = LoadActor("../Pane2/Percentage.lua", player)
 
 pane[#pane+1] = LoadFont("Common Normal")..{
 	Text="GrooveStats QR",
@@ -96,27 +99,13 @@ pane[#pane+1] = Def.Quad{
 	InitCommand=function(self) self:xy(text_x_offset, text_y_offset+20):zoomto(text_width-3,1):align(0,0):diffuse(1,1,1,0.33) end
 }
 
--- if there are multiple reasons the score was invalid for GrooveStats ranking
--- the help text might spill outside the vertical bounds of the pane
--- hide any such spillover with a mask
-if not ValidForGrooveStats then
-	pane[#pane+1] = Def.Quad{
-		InitCommand=function(self) self:xy(-10, 142):zoomto(121,140):align(0,0):MaskSource() end
-	}
-end
-
 -- localized help text, either "use your phone to scan" or "here's why your score was invalid"
 pane[#pane+1] = LoadFont("Common Normal")..{
 	Text=text,
 	InitCommand=function(self)
-		self:zoom(text_body_size):xy(text_x_offset,text_y_offset+27):align(0,0):vertspacing(-4):wrapwidthpixels(text_width/text_body_size)
+		self:zoom(text_body_size):xy(text_x_offset,text_y_offset+27):align(0,0):vertspacing(-4):wrapwidthpixels(text_width/text_body_size):MaskDest()
 
 		local z = ValidForGrooveStats and 0.8 or 0.675
-		self:zoom(z)
-		self:y( scale(35, 0,0.8,   0,z) )
-		self:x( scale(-4, 0,0.675, 0,z) )
-		self:wrapwidthpixels( scale(98, 0,0.675, 0,z)/z)
-
 		-- FIXME: Oof.
 		if THEME:GetCurLanguage() == "ja" then self:_wrapwidthpixels( scale(96, 0,0.8, 0,z)/z ) end
 	end,
