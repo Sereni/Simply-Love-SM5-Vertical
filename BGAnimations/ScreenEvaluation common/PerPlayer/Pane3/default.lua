98b466ab1313ab53ccb578f20cdc7358a753dd25
Sereni <sereni.nm@gmail.com>
2020-07-01 07:43:28 +0100

    Fix alignment of personal high scores

--- BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/Pane3/default.lua
@@ -15,38 +15,31 @@ local pane = Def.ActorFrame{
 }
 
 -- row_height of a HighScore line
-local rh
+local rh = 20
 local args = { Player=player, RoundsAgo=1, RowHeight=rh}
 
 -- if the player is using a profile (local or USB)
 if PROFILEMAN:IsPersistentProfile(player) then
 
-	-- less line spacing between HighScore rows to fit the horizontal line
-	rh = 20.25
-	args.RowHeight = rh
-
-	-- top 7 machine HighScores
-	args.NumHighScores = 5
+	-- top 5 machine HighScores
+	numHighScores = 5
+	args.NumHighScores = numHighScores
 	pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)
 
 	-- horizontal line visually separating machine HighScores from player HighScores
-	pane[#pane+1] = Def.Quad{ InitCommand=function(self) self:zoomto(100, 1):y(rh*8):diffuse(1,1,1,0.33) end }
+	pane[#pane+1] = Def.Quad{ InitCommand=function(self) self:zoomto(100, 1):y(rh*(numHighScores+1)):diffuse(1,1,1,0.33) end }
 
 	-- top 3 player HighScores
 	args.NumHighScores = 3
 	args.Profile = PROFILEMAN:GetProfile(player)
 	pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)..{
-		InitCommand=function(self) self:y(rh*8) end
+		InitCommand=function(self) self:y(rh*(numHighScores+1)) end
 	}
 
 -- else the player is not using a profile
 else
-	-- more breathing room between HighScore rows
-	rh = 22
-	args.RowHeight = rh
-
-	-- top 10 machine HighScores
-	args.NumHighScores = 10
+	-- top 9 machine HighScores
+	args.NumHighScores = 9
 	pane[#pane+1] = LoadActor(THEME:GetPathB("", "_modules/HighScoreList.lua"), args)
 end
 
