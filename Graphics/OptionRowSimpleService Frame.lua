0d5e5a6c0df5660b1d7332fdefcc40de945f3700
Sereni <sereni.nm@gmail.com>
2020-07-13 09:11:08 +0100

    Realign service menu options

--- Graphics/OptionRowSimpleService Frame.lua
+++ Graphics/OptionRowSimpleService Frame.lua
@@ -1,9 +1,6 @@
 local row_height = 30
-local row_width = {
-	active   = WideScale(304, 460),
-	inactive = WideScale(300, 456)
-}
-
+local row_width = 358.6
+local row_bg_x = _screen.cx+92
 
 local t = Def.ActorFrame{}
 t.InitCommand=function(self) self:x(WideScale(12, 30)) end
@@ -11,10 +8,9 @@ t.InitCommand=function(self) self:x(WideScale(12, 30)) end
 -- a row
 t[#t+1] = Def.Quad {
 	Name="RowBackgroundQuad",
-	InitCommand=function(self) self:zoomto(row_width.active, row_height):horizalign(left) end,
-	OnCommand=function(self) self:diffusealpha(1) end,
-	GainFocusCommand=function(self) self:zoomtowidth(row_width.active)   end,
-	LoseFocusCommand=function(self) self:zoomtowidth(row_width.inactive) end
+	InitCommand=function(self)
+		self:x(row_bg_x):horizalign(center):zoomto(row_width, row_height)
+	end,
 }
 
 return t
