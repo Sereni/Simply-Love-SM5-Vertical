f538c8285ca62b174bfd8dc7d35309b2580b40b2
Sereni <sereni.nm@gmail.com>
2020-07-13 08:41:52 +0100

    Revert OptionRowServiceChild to a working state

--- Graphics/OptionRowServiceChild Frame.lua
+++ Graphics/OptionRowServiceChild Frame.lua
@@ -1,11 +1,9 @@
+local row_bg_width = 358
+local row_bg_height = 30
+local row_bg_x = _screen.cx+92
+
 local title_bg_width = 115
 local title_bg_x = row_bg_x - row_bg_width/2
-
-local row_height = 30
-local row_width = {
-	active   = 358,
-	inactive = 354
-}
 
 local t = Def.ActorFrame{}
 t.InitCommand=function(self) self:x(WideScale(12, 30)) end
@@ -13,16 +11,16 @@ t.InitCommand=function(self) self:x(WideScale(12, 30)) end
 -- a row
 t[#t+1] = Def.Quad {
 	Name="RowBackgroundQuad",
-	InitCommand=function(self) self:zoomto(row_width.active, row_height):horizalign(left) end,
-	GainFocusCommand=function(self) self:zoomtowidth(row_width.active)   end,
-	LoseFocusCommand=function(self) self:zoomtowidth(row_width.inactive) end
+	InitCommand=function(self)
+		self:horizalign(center):x(row_bg_x):zoomto(row_bg_width, row_bg_height)
+	end,
 }
 
 -- black quad behind the title
 t[#t+1] = Def.Quad {
 	Name="TitleBackgroundQuad",
 	OnCommand=function(self)
-		self:zoomto(title_bg_width, row_height)
+		self:zoomto(title_bg_width, row_bg_height)
 			:halign(0):x(title_bg_x)
 			:diffuse(Color.Black):diffusealpha( DarkUI() and 0.75 or 0.25)
 	end
