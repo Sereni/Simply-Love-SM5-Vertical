35a18cc75ddda91a200863c11d6664c8c4cfc9f2
Sereni <sereni.nm@gmail.com>
2020-07-23 17:10:34 +0100

    Darken density graph background to make light blue graph more visible

--- BGAnimations/ScreenSelectMusic overlay/DensityGraph.lua
+++ BGAnimations/ScreenSelectMusic overlay/DensityGraph.lua
@@ -45,7 +45,7 @@ return Def.ActorFrame {
         InitCommand=function(self)
             self:zoomto(histogramWidth,histogramHeight)
                 :align(0, 0)
-                :diffuse(color("#4D6677"))
+                :diffuse(color("#0a141b"))
         end
     },
 
