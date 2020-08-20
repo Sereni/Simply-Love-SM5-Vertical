bcbb257636fd2acd985aca06eedb57507138f656
Sereni <sereni.nm@gmail.com>
2020-08-20 20:51:19 +0100

    Add autoscroll to preferred songs

--- BGAnimations/ScreenSelectPlayMode out.lua
+++ BGAnimations/ScreenSelectPlayMode out.lua
@@ -1,5 +1,32 @@
 return Def.Actor{
 	OffCommand=function(self)
 		self:sleep(0.9)
+
+		if ECS.Mode == "ECS" then
+			if PlayerIsUpper() == nil then return end
+
+			if PlayerIsUpper() then
+				local omlaah = SONGMAN:FindSong("ECS9 - Upper/[18] [186] One More Lovely (After After Hours) FP 186")
+				if omlaah then
+					GAMESTATE:SetPreferredSong(omlaah)
+				end
+			else
+				local discovery = SONGMAN:FindSong("ECS9 - Lower/[12] [130] Discovery")
+				if discovery then
+					GAMESTATE:SetPreferredSong(discovery)
+				else
+					local ians_op = SONGMAN:FindSong("ECS9 - Lower/[17] [174] Ian's OP")
+					if ians_op then
+						GAMESTATE:SetPreferredSong(ians_op)
+					end
+				end
+			end
+		elseif ECS.Mode == "Marathon" then
+			-- TODO(teejusb)
+			local masochisma = SONGMAN:FindSong("ECS9 - Upper Marathon/Masochisma Mk 0")
+			if masochisma then
+				GAMESTATE:SetPreferredSong(masochisma)
+			end
+		end
 	end
 }
