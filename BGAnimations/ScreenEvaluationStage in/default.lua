3e9bd71cf98fa7b1af79a1192b0fdd1a6b23950a
Sereni <sereni.nm@gmail.com>
2020-08-20 20:14:27 +0100

    Apply and cancel relic effects in ScreenEvaluationStage in

--- BGAnimations/ScreenEvaluationStage in/default.lua
+++ BGAnimations/ScreenEvaluationStage in/default.lua
@@ -1,6 +1,12 @@
 -- assume that all human players failed
 local img = "failed text.png"
 
+local ApplyRelicActions = function()
+	for active_relic in ivalues(ECS.Player.Relics) do
+		active_relic.action()
+	end
+end
+
 -- loop through all available human players
 for player in ivalues(GAMESTATE:GetHumanPlayers()) do
 	-- if any of them passed, we want to display the "cleared" graphic
@@ -10,6 +16,16 @@ for player in ivalues(GAMESTATE:GetHumanPlayers()) do
 end
 
 return Def.ActorFrame {
+	OnCommand=function(self) ApplyRelicActions() end,
+	OffCommand=function(self)
+		-- always undo the effects of Astral Ring/Astral Earring when leaving ScreenEval, even if they weren't active
+		SL.Global.ActiveModifiers.TimingWindows = {true,true,true,true,true}
+		PREFSMAN:SetPreference("TimingWindowSecondsW4", SL.Preferences.ITG.TimingWindowSecondsW4)
+		PREFSMAN:SetPreference("TimingWindowSecondsW5", SL.Preferences.ITG.TimingWindowSecondsW5)
+
+		-- always undo the effects of any relics that change LifeDifficulty when leaving ScreenEval, even if they weren't active
+		PREFSMAN:SetPreference("LifeDifficultyScale", 1)
+	end,
 	Def.Quad{
 		InitCommand=function(self) self:FullScreen():diffuse(Color.Black) end,
 		OnCommand=function(self) self:sleep(0.2):linear(0.5):diffusealpha(0) end,
