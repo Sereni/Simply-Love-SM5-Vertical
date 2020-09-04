432565f9bff11cb3bb478987ccc75499e80d7ca2
Sereni <sereni.nm@gmail.com>
2020-09-04 13:04:52 +0100

    Add UI and logic for Faust's Scalpel

--- BGAnimations/ScreenGameplay overlay/ECS.lua
+++ BGAnimations/ScreenGameplay overlay/ECS.lua
@@ -160,7 +160,8 @@ end
 -- ----------------------------------------------------------
 -- actually hook into the screen so that we can do thing at screen's OnCommand and OffCommand
 
-return Def.Actor{
+local af = Def.ActorFrame{}
+af[#af+1] = Def.Actor{
 	OnCommand=function(self)
 		if ECS.Mode == "ECS" or ECS.Mode == "Marathon" then
 			-- relic actions depend on the current screen,
@@ -169,7 +170,7 @@ return Def.Actor{
 
 			ExpendChargesOnActiveRelics()
 		end
- 
+
 		SCREENMAN:GetTopScreen():AddInputCallback(InputHandler)
 	end,
 	OffCommand=function(self)
@@ -190,3 +191,152 @@ return Def.Actor{
 		end
 	end
 }
+
+-- -----------------------------------------------------------------------
+-- prefer the engine's SecondsToHMMSS()
+-- but define it ourselves if it isn't provided by this version of SM5
+local mmss = "%02d:%02d"
+
+local SecondsToMMSS = function(s)
+	-- native floor division sounds nice but isn't available in Lua 5.1
+	local mins  = math.floor((s % 3600) / 60)
+	local secs  = s - (mins * 60)
+	return mmss:format(mins, secs)
+end
+
+local FaustsScalpelIsActive = function()
+	if true then return true end
+	for active_relic in ivalues(ECS.Player.Relics) do
+		if active_relic.name == "Faust's Scalpel" then
+			return true
+		end
+	end
+	return false
+end
+
+local second_to_pause = 8
+local pause_duration_seconds = 10
+local elapsed_seconds = 0
+
+-- Hopefully no one ratemods the marathon but you never know.
+local rate = SL.Global.ActiveModifiers.MusicRate
+
+local InputHandler = function(event)
+	if not event.PlayerNumber or not event.button then return false end
+
+	if event.type == "InputEventType_FirstPress" and event.GameButton == "Start" then
+		MESSAGEMAN:Broadcast("UnpauseMarathon")
+	end
+
+	return false
+end
+
+local IsPlayingMarathon = function()
+	local song = GAMESTATE:GetCurrentSong()
+	local group_name = song:GetGroupName()
+	local song_name = song:GetMainTitle()
+	return group_name == "ECS9 - Upper Marathon" and song_name == "Stratospheric Intricacy"
+end
+
+if ECS.Mode == "Marathon" and FaustsScalpelIsActive() and IsPlayingMarathon() then
+	af[#af+1] = Def.ActorFrame{
+		InitCommand=function(self) end,
+		OnCommand=function(self) SCREENMAN:GetTopScreen():AddInputCallback( InputHandler ) self:queuecommand("Loop") end,
+		LoopCommand=function(self)
+			if GAMESTATE:GetNumPlayersEnabled() == 1 then
+				local cur_second = GAMESTATE:GetPlayerState(player):GetSongPosition():GetMusicSeconds() / rate
+				if cur_second >= second_to_pause then
+					self:queuecommand("PauseMarathon")
+				else
+					self:sleep(0.1):queuecommand("Loop")
+				end
+			end
+		end,
+		PauseMarathonCommand=function(self)
+			SCREENMAN:GetTopScreen():PauseGame(true)
+			self:queuecommand("Wait")
+		end,
+		WaitCommand=function(self)
+			if SCREENMAN:GetTopScreen():IsPaused() then
+				elapsed_seconds = elapsed_seconds + 1
+				if elapsed_seconds < pause_duration_seconds then
+					self:sleep(1):queuecommand("Wait")
+				else
+					MESSAGEMAN:Broadcast("UnpauseMarathon")
+				end
+			end
+		end,
+		UnpauseMarathonMessageCommand=function(self)
+			if SCREENMAN:GetTopScreen():IsPaused() then
+				SCREENMAN:GetTopScreen():PauseGame(false)
+			end
+		end,
+
+		Def.ActorFrame {
+			InitCommand=function(self) self:visible(false) end,
+			WaitCommand=function(self)
+				if SCREENMAN:GetTopScreen():IsPaused() then
+					self:visible(true)
+				end
+			end,
+			UnpauseCommand=function(self) self:visible(false) end,
+			-- slightly darken the entire screen
+			Def.Quad {
+				InitCommand=function(self) self:FullScreen():diffuse(Color.Black):diffusealpha(0.4) end
+			},
+			Def.Quad {
+				InitCommand=function(self) self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):diffuse(Color.White):zoomto(202, 202) end
+			},
+			Def.Quad {
+				InitCommand=function(self) self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y):diffuse(Color.Black):zoomto(200, 200) end
+			},
+			LoadFont("Common Normal")..{
+				InitCommand=function(self)
+					local text = "You may end your break time early by pressing the &START; button"
+					self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y+40):wrapwidthpixels(200):settext(text)
+				end,
+			}
+		},
+
+		LoadFont("Common Normal")..{
+			InitCommand=function(self)
+				self:horizalign(left):xy(_screen.cx-70, 57):zoom(0.8)
+			end,
+			LoopCommand=function(self)
+				local cur_second = GAMESTATE:GetPlayerState(player):GetSongPosition():GetMusicSeconds() / rate
+				if cur_second > 0 then
+					if cur_second < second_to_pause then
+						self:settext(SecondsToMMSS(second_to_pause - cur_second + 1))
+					end
+				end
+			end,
+			WaitCommand=function(self)
+				local remaining_pause_duration = pause_duration_seconds - elapsed_seconds + 1
+				self:horizalign(center):xy(SCREEN_CENTER_X, SCREEN_CENTER_Y-25):zoom(1)
+				if remaining_pause_duration >= 0 then
+					if remaining_pause_duration <= 5 then
+						self:diffuse(color("#ff3030"))
+					end
+					self:settext(SecondsToMMSS(remaining_pause_duration))
+				end
+			end,
+			UnpauseMarathonMessageCommand=function(self)
+				self:visible(false)
+			end,
+		},
+		LoadFont("Common Normal")..{
+			InitCommand=function(self)
+				self:horizalign(right):xy(_screen.cx-77, 57):settext("Pausing in"):zoom(0.8):visible(true)
+			end,
+			WaitCommand=function(self)
+				self:horizalign(center):xy(SCREEN_CENTER_X, SCREEN_CENTER_Y-50):zoom(1)
+				self:settext("Unpausing in")
+			end,
+			UnpauseMarathonMessageCommand=function(self)
+				self:visible(false)
+			end,
+		}
+	}
+end
+
+return af
