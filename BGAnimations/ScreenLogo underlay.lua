dd7de69cc01a6dc9eeedcb7d2237e27fd4c50926
Sereni <sereni.nm@gmail.com>
2020-09-12 12:49:10 +0100

    Center the ECS logo in ScreenLogo underlay
    (which Sereni doesn't know where it is but okay)

--- BGAnimations/ScreenLogo underlay.lua
+++ BGAnimations/ScreenLogo underlay.lua
@@ -1,73 +1,14 @@
-local image = ThemePrefs.Get("VisualTheme")
-local game = GAMESTATE:GetCurrentGame():GetName()
-if game ~= "dance" and game ~= "pump" then
-	game = "techno"
-end
-
--- - - - - - - - - - - - - - - - - - - - -
--- Constants defining UI element positions depending on horizontal or vertical mode.
-local logos_x			= (IsVerticalScreen() and 2 or 0)
-local logos_y			= (IsVerticalScreen() and -10 or -16)
-local logos_zoom_itg		= (IsVerticalScreen() and 0.114 or 0.2)
-local logos_zoom_pump		= (IsVerticalScreen() and 0.125 or 0.205)
-local title_zoom		= (IsVerticalScreen() and 0.4 or 0.7)
-
-local t = Def.ActorFrame{
+-- ECS Logo
+local af = Def.ActorFrame{
 	InitCommand=function(self)
-		self:xy(_screen.cx, _screen.cy-16):zoom( game=="pump" and 0.2 or 0.205 ):cropright(1)
+		self:Center()
 	end,
-
-	LoadActor(THEME:GetPathG("", "_logos/" .. game))..{
-		InitCommand=function(self)
-			self:xy(_screen.cx+logos_x, logos_y):zoom( game=="pump" and logos_zoom_itg or logos_zoom_pump ):cropright(1)
-		end,
-		OnCommand=function(self)
-			self:linear(0.33):cropright(0)
-		end
-	},
-
-	LoadActor(THEME:GetPathG("", "_VisualStyles/".. image .."/TitleMenu (doubleres).png"))..{
-		InitCommand=function(self)
-			self:x(_screen.cx+2):diffusealpha(0):zoom(title_zoom)
-				:shadowlength(1)
-		end,
-		OnCommand=cmd(linear,0.5; diffusealpha, 1)
-	}
+	OffCommand=function(self) self:linear(0.5):diffusealpha(0) end,
 }
 
-t[#t+1] = LoadActor(THEME:GetPathG("", "_VisualStyles/".. image .."/TitleMenu (doubleres).png"))..{
-	InitCommand=function(self)
-		self:xy(_screen.cx+2, _screen.cy):diffusealpha(0):zoom(0.7)
-			:shadowlength(1)
-	end,
-	OffCommand=cmd(visible,false),
-
-	LoadFont("Wendy/_wendy small")..{
-		Text=THEME:GetString("ScreenLogo", "EnterCreditsToPlay"),
-		InitCommand=cmd(xy,_screen.cx,SCREEN_BOTTOM-100; zoom,0.525; visible,false),
-		RefreshCommand=function(self)
-			local credits = GetCredits()
-			self:visible( GAMESTATE:GetCoinMode() == "CoinMode_Pay" and credits.Credits <= 0 )
-		end
-	},
-
-	LoadFont("Wendy/_wendy small")..{
-		Text=THEME:GetString("ScreenTitleJoin", "Press Start"),
-		InitCommand=cmd(xy,_screen.cx, _screen.h-80; zoom,0.715; visible,false),
-		RefreshCommand=function(self)
-			local credits = GetCredits()
-			self:visible( (GAMESTATE:GetCoinMode() == "CoinMode_Pay" and credits.Credits > 0) or GAMESTATE:GetCoinMode() == "CoinMode_Free")
-		end
-	},
-
-	LoadFont("Wendy/_wendy small")..{
-		Text=THEME:GetString("ScreenSelectMusic","Start Button"),
-		InitCommand=cmd(x,_screen.cx - 12; y,_screen.h - 125; zoom,1.1; visible,false),
-		RefreshCommand=function(self)
-			local credits = GetCredits()
-			self:visible( (GAMESTATE:GetCoinMode() == "CoinMode_Pay" and credits.Credits > 0) or GAMESTATE:GetCoinMode() == "CoinMode_Free")
-		end
-	}
+af[#af+1] = LoadActor(THEME:GetPathG("", "_logos/ecs (doubleres).png"))..{
+	InitCommand=function(self) self:xy(2,10):zoom(0.25):shadowlength(0.75) end,
+	OffCommand=function(self) self:linear(0.5):shadowlength(0) end
 }
 
-return t
+return af
