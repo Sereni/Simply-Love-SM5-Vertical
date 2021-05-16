941fa1918fcf54973cf2a28633c1bdf2a59438e2
Sereni <sereni.nm@gmail.com>
2021-05-16 08:06:02 +0100

    Fail players when holding the start button

--- BGAnimations/ScreenGameplay overlay/default.lua
+++ BGAnimations/ScreenGameplay overlay/default.lua
@@ -17,6 +17,7 @@
 local af = Def.ActorFrame{}
 
 af[#af+1] = LoadActor("./WhoIsCurrentlyWinning.lua")
+af[#af+1] = LoadActor("./FailOnHoldStart.lua")
 
 for player in ivalues( GAMESTATE:GetHumanPlayers() ) do
 
