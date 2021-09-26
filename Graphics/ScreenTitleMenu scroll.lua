1101282c585e3bcb0ccaeb3e99386e4510ad0f7d
Sereni <sereni.nm@gmail.com>
2021-09-26 07:52:15 +0100

    Update ScreenTitleMenu font colors

--- Graphics/ScreenTitleMenu scroll.lua
+++ Graphics/ScreenTitleMenu scroll.lua
@@ -11,8 +11,8 @@ t[#t+1] = LoadFont("Common Bold")..{
 	OnCommand=function(self) self:diffusealpha(0):sleep(index*0.075):linear(0.2):diffusealpha(1) end,
 	OffCommand=function(self) self:sleep(index*0.075):linear(0.18):diffusealpha(0) end,
 
-	GainFocusCommand=function(self) self:stoptweening():zoom(0.5):accelerate(0.1):diffuse(PlayerColor(PLAYER_2)):glow(1,1,1,0.5):decelerate(0.05):glow(1,1,1,0) end,
-	LoseFocusCommand=function(self) self:stoptweening():zoom(0.4):accelerate(0.1):diffuse(ThemePrefs.Get("RainbowMode") and {1,1,1,1} or color("#888888")):glow(1,1,1,0) end
+	GainFocusCommand=function(self) self:stoptweening():zoom(0.5):accelerate(0.1):diffuse(color("#e88300")):glow(1,1,1,0.5):decelerate(0.05):glow(1,1,1,0) end,
+	LoseFocusCommand=function(self) self:stoptweening():zoom(0.4):accelerate(0.1):diffuse(color("#fbff94")):glow(1,1,1,0) end
 }
 
 return t
